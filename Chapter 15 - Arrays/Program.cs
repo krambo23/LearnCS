134a34e7929bbe2aaaf5fcaf381fbb2d43fec65b
Keshav Rambhujoo <kr@valstream.com>
2022-05-11 18:17:09 +0100

    Chapter 15

---
+++ Chapter 15 - Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ArrayApplication
+{
+    class MyArray
+    {
+        static void Main(string[] args)
+        {
+            // Initialise Array
+            double[] balance = new double[10];
+
+            // Assigning Values to the Array
+            balance[0] = 4500.0;
+
+            // Access Array Elements
+            double salary = balance[9];
+        }
+    }
+}
+// pg 107, 108
