8143e0d6caf599b5fc6654cae769d69eb2ef5e12
Keshav Rambhujoo <kr@valstream.com>
2022-05-11 23:38:14 +0100

    Chapter 18

---
+++ Chapter 18 - Enums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumApplication
+{
+    class EnumProgram
+    {
+        enum Days
+        {
+            Sunday, 
+            Monday, 
+            Tuesday, 
+            Wednesday, 
+            Thursday, 
+            Friday, 
+            Saturday
+        };
+
+        static void Main(string[] args)
+        {
+            int weekStart = (int) Days.Monday;
+            int weekEnd = (int) Days.Friday;
+            System.Console.WriteLine("Monday: {0}", weekStart);
+            System.Console.WriteLine("Friday: {0}", weekEnd);
+        }
+    }
+}
