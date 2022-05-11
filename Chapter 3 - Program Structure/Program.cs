5bfafd769f1cbdf578de53a9bb3f8185aea63015
Keshav Rambhujoo <kr@valstream.com>
2022-05-11 12:49:57 +0100

    Chapter 3

---
+++ Chapter 3 - Program Structure/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[]  args) 
+        {
+            /* My First Program in C# */
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
