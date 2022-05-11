6621f8a29c9b2ba1d57e3cd5f36235152844892c
Keshav Rambhujoo <kr@valstream.com>
2022-05-11 14:23:29 +0100

    Chapter 8

---
+++ Chapter 8 - Constants and Literals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConstantsAndLiterals
+{
+    class Program
+    {
+        static void EscapeCharacters()
+        {
+            System.Console.WriteLine("Hello\tWorld\n\n");
+        }
+
+        static void DeclaringConstants()
+        {
+            // Declaring Constant
+            const double PI = 3.14159;
+
+            double radius;
+
+            System.Console.Write("Input Radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            double areaOfCircle = PI * radius * radius;
+            System.Console.WriteLine("Radius = {0}\nArea: {1}", radius, areaOfCircle);
+        }
+
+        static void Main(string[] args)
+        {
+            EscapeCharacters();
+
+            DeclaringConstants();
+        }
+    }
+}
+// pg 30, 31
