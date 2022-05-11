0e37333ad88c274947b273ab3c7898dc30e19062
Keshav Rambhujoo <kr@valstream.com>
2022-05-11 16:25:44 +0100

    Chapter 10

---
+++ Chapter 10 - Decision Making/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DecisionMaking
+{
+    class Program
+    {
+        static void IfStatements()
+        {
+            int a = 10;
+
+            // 'a' is less than 20
+            if (a < 20)
+            {
+                System.Console.WriteLine("a < 20");
+            }
+            else if (a > 20) 
+            {
+                System.Console.WriteLine("a > 20");
+            }
+            else
+            {
+                System.Console.WriteLine("a == 20");
+            }
+        }
+
+        static void SwitchStatements()
+        {
+            char grade = 'A';
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("Excellent!");
+                    break;
+                case 'B':
+                case 'C':
+                    Console.WriteLine("Well done");
+                    break;
+                case 'D':
+                    Console.WriteLine("You passed");
+                    break;
+                case 'F':
+                    Console.WriteLine("Better try again");
+                    break;
+                default:
+                    Console.WriteLine("Invalid grade");
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IfStatements();
+            System.Console.WriteLine();
+            SwitchStatements();
+        }
+    }
+}
+// pg 55
+// pg 62
