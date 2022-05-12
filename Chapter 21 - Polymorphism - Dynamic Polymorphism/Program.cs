20ce9b1089f049ced4b243d4ddc589471dada967
Keshav Rambhujoo <kr@valstream.com>
2022-05-12 01:05:17 +0100

    Chapter 21

---
+++ Chapter 21 - Polymorphism - Dynamic Polymorphism/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PolymorphismApplication
+{
+    abstract class Shape
+    {
+        public abstract int area();
+    }
+
+    class Rectangle : Shape
+    {
+        private int length;
+        private int width;
+
+        public Rectangle(int a = 0, int b = 0)
+        {
+            length = a;
+            width = b;
+        }
+
+        // Implementing Method To Calculate Area
+        public override int area()
+        {
+            return width * length;
+        }
+    }
+
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(10, 7);
+            double area = rectangle.area();
+
+            System.Console.WriteLine($"Area: {area}");
+        }
+    }
+}
+// pg 165, 166
---
+++ Chapter 21 - Polymorphism - Static Polymorphism/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace PolymorphismApplication
+{
+    class PrintData
+    {
+        void Print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i );
+        }
+
+        void Print(double f)
+        {
+            Console.WriteLine("Printing float: {0}" , f);
+        }
+
+        void Print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+
+        static void Main(string[] args)
+        {
+            PrintData p = new PrintData();
+
+            // Call Print to print integer
+            p.Print(5);
+
+            // Call Print to print float
+            p.Print(500.263);
+
+            // Call Print to print string
+            p.Print("Hello C#");
+        }
+    }
+}
+// pg 163, 164
