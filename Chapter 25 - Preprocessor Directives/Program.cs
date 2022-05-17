d7fadd147e53233335733a87ddd3e847f9bb4710
Keshav Rambhujoo <kr@valstream.com>
2022-05-17 22:40:56 +0100

    Chapter 25

---
+++ Chapter 25 - Preprocessor Directives/Program.cs
@@ -0,0 +1,19 @@
+﻿#define PI
+
+using System;
+
+namespace PreprocessorDirectivesApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            #if (PI)
+                System.Console.WriteLine("PI is Defined!");
+            #else
+                System.Console.WriteLine("PI is Not Defined!");
+            #endif
+        }
+    }    
+}
+// pg 191
