439f9937cc63bbbce4bbefdce68981242e07ebb7
Keshav Rambhujoo <kr@valstream.com>
2022-05-17 22:29:28 +0100

    Chapter 24

---
+++ Chapter 24 - Namespaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System; // Importing Namespace 'System'
+
+namespace first_namespace
+{
+    class namespace_c1
+    {
+        public void func()
+        {
+            System.Console.WriteLine("Inside first_space");
+        }
+    }
+}
+
+namespace second_space
+{
+    class namespace_c1
+    {
+        public void func()
+        {
+            System.Console.WriteLine("Inside second_space");
+        }
+    }
+}
+
+class TestClass
+{
+    public static void Main(string[] args)
+    {
+        first_namespace.namespace_c1 fc = new first_namespace.namespace_c1();
+        second_space.namespace_c1 sc = new second_space.namespace_c1();
+
+        fc.func();
+        sc.func();
+    }
+}
+// pg 184, 185
