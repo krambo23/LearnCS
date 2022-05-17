b1d7ed93d3c6900173748f6f06887a105e195b97
Keshav Rambhujoo <kr@valstream.com>
2022-05-18 00:39:27 +0100

    Chapter 29

---
+++ Chapter 29 - Attributes/Program.cs
@@ -0,0 +1,50 @@
+﻿#define DEBUG
+
+using System;
+using System.Diagnostics;
+
+public class MyClass
+{
+    [Conditional("DEBUG")]
+    public static void Message(string msg)
+    {
+        System.Console.WriteLine(msg);
+    }
+
+    // If Second Parameter is 'true', Will Throw Error
+    // If Second Parameter is 'false', Will Display Warning
+    [Obsolete("Don't Use OldMethod - Use NewMethod Instead", true)]
+    public static void OldMethod()
+    {
+        System.Console.WriteLine("Old Method");
+    }
+
+    public static void NewMethod()
+    {
+        System.Console.WriteLine("New Method");
+    }
+}
+
+class Test
+{
+    static void function1()
+    {
+        MyClass.Message("FN1");
+        function2();
+    }
+
+    static void function2()
+    {
+        MyClass.Message("FN2");
+    }
+
+    public static void Main(string[] args)
+    {
+        MyClass.Message("Main FN");
+        function1();
+
+        MyClass.OldMethod();
+    }
+}
+// pg 236
+// pg 237, 238
---
+++ Chapter 29 - Custom Attributes/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+// Custom Attributes
+[AttributeUsage(
+    AttributeTargets.Class |
+    AttributeTargets.Constructor |
+    AttributeTargets.Field |
+    AttributeTargets.Method |
+    AttributeTargets.Property,
+    AllowMultiple = true)
+]
+
+public class DebugInfo : System.Attribute
+{
+    private int bugNo;
+    private string developer;
+    private string lastReview;
+    public string message;
+
+    public DebugInfo(int bg, string dev, string d)
+    {
+        this.bugNo = bg;
+        this.developer = dev;
+        this.lastReview = d;
+    }
+
+    public int BugNo { get; private set; }
+    public string Developer { get; private set; }
+    public string LastReview { get; private set; }
+    public string Message { get; set; }
+}
+
+[DebugInfo(420, "Elon Musk", "28/06/1971", Message = "Return type mismatch")]
+[DebugInfo(69, "Bart Simspon", "23/02/2012", Message = "Unused variable")]
+class Rectangle
+{
+    protected double length;
+    protected double width;
+
+    public Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+
+    [DebugInfo(55, "Zara Ali", "19/10/2012", Message = "Return type mismatch")]
+    public double GetArea()
+    {
+        return length * width;
+    }
+
+    [DebugInfo(56, "Zara Ali", "19/10/2012")]
+    public void Display()
+    {
+        Console.WriteLine("Length: {0}", length);
+        Console.WriteLine("Width: {0}", width);
+        Console.WriteLine("Area: {0}", GetArea());
+    }
+
+    public static void Main(string[] args)
+    {
+        Rectangle rectangle = new Rectangle(10, 20);
+        rectangle.Display();
+    }
+}
+// pg 239 - 242
