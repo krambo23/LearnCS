5d93e8b84c5679a3f9d931bc430a18bbf448a91d
Keshav Rambhujoo <kr@valstream.com>
2022-05-17 23:13:01 +0100

    Chapter 27

---
+++ Chapter 27 - Exception Handling - User Defined Exceptions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace UserDefinedException
+{
+    class TestTemperature
+    {
+        public static void Main(string[] args)
+        {
+            Temperature temperature = new Temperature();
+            try
+            {
+                temperature.showTemp();
+            }
+            catch (TempIsZeroException e)
+            {
+                System.Console.WriteLine($"TempIsZeroException: {e.Message}");
+            }
+        }
+    }
+}
+
+public class TempIsZeroException : ApplicationException
+{
+    public TempIsZeroException(string message) : base (message)
+    {
+
+    }
+}
+
+public class Temperature
+{
+    int temperature = 0;
+
+    public void showTemp()
+    {
+        if (temperature == 0)
+        {
+            throw new TempIsZeroException("Zero Temperature Found!");
+        }
+        else 
+        {
+            System.Console.WriteLine($"Temperature : {temperature}");
+        }
+    }
+}
+// pg 212, 213
---
+++ Chapter 27 - Exception Handling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ErrorHandlingApplication
+{
+    class DivNumbers
+    {
+        int result;
+
+        DivNumbers()
+        {
+            result = 0;
+        }
+
+        public void division(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                System.Console.WriteLine($"Exception Caught : {e}");
+            }
+            finally
+            {
+                System.Console.WriteLine($"Result : {result}");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            DivNumbers d1 = new DivNumbers();
+            d1.division(20, 5);
+
+            System.Console.WriteLine();
+            
+            DivNumbers d2 = new DivNumbers();
+            d2.division(10, 0);
+        }
+    }
+}
+// pg 210, 211
